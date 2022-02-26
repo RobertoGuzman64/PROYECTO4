b8895fd2fe9e9ec5c259236671a01d4c765a9c5d
RobertoGuzman <robertoguzman64@gmail.com>
2022-02-26 23:09:10 +0100

    Añadido nuevo modelo de Pagos y los requisitos en la migracion para que estos se puedan realizar

---
+++ migrations/20220226220322-create-pago.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Pagos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      tarjeta: {
+        type: Sequelize.STRING
+      },
+      paypal: {
+        type: Sequelize.STRING
+      },
+      usuarioId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Usuarios',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Pagos');
+  }
+};
