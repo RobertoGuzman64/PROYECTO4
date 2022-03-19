a9ddbf6dcab7a9adba0b449fb649d6a6871764f3
RobertoGuzman <robertoguzman64@gmail.com>
2022-03-19 13:27:06 +0100

    Añadido isAdmin al endpoint de ver todos los Usuarios

--- views/UsuarioRouter.js
+++ views/UsuarioRouter.js
@@ -8,12 +8,8 @@ const UsuarioController = require('../controllers/UsuarioController');
 //AQUI ES DONDE CREAMOS LAS RUTAS DE LAS FUNCIONES DE USUARIOCONTROLLER.
 
 // Endpoint de lista de todos los Usuarios.
-router.get('/', auth, isAdmin, UsuarioController.verTodos);
+router.get('/admin', auth, isAdmin ,UsuarioController.verTodos);
 // http://localhost:5000/usuarios/
-
-// Endpoint de busqueda de un Usuario por ID.
-router.get('/:id', auth, UsuarioController.verPorId);
-// http://localhost:5000/usuarios/id
 
 // Endpoint de registrar Usuario.
 router.post('/', UsuarioController.registraUsuario);
@@ -23,10 +19,6 @@ router.post('/', UsuarioController.registraUsuario);
 router.post('/login', UsuarioController.loginUsuario);
 // http://localhost:5000/usuarios/login
 
-// Endpoint de Modificar el perfil por ID.
-router.put('/:id', auth, UsuarioController.perfilUsuario);
-// http://localhost:5000/usuarios/:id
-
 // Endpoint de borrar todos los Usuarios.
 router.delete('/', auth, UsuarioController.borrarTodo);
 // http://localhost:5000/usuarios/
@@ -35,8 +27,13 @@ router.delete('/', auth, UsuarioController.borrarTodo);
 router.delete('/:id', auth, isAdmin, UsuarioController.borrarPorId);
 // http://localhost:5000/usuarios/:id
 
+// Endpoint de busqueda de un Usuario por ID.
+router.get('/:id', auth, UsuarioController.verPorId);
+// http://localhost:5000/usuarios/id
 
-
+// Endpoint de Modificar el perfil por ID.
+router.put('/:id', auth, UsuarioController.perfilUsuario);
+// http://localhost:5000/usuarios/:id
 
 
 
